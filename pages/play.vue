9191e3ef20cb3c8224a77684a64594a859fe507b
chenlin-wang <wangchenlin2001@icloud.com>
2024-10-01 05:21:45 +0000

    backup 3

--- pages/play.vue
+++ pages/play.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import * as Tone from "tone";
 import { Samples } from "../shared/samples";
+import { conbinations, Key, updateBasicInotonations } from "~/shared/temperament";
 
 definePageMeta({
     layout: "keyboard"
@@ -17,6 +18,10 @@ const release = (note: string) => {
     console.log(`release: ${note}`)
     synth.triggerRelease([note])
 }
+
+// updateBasicInotonations(Key.A)
+
+console.log(conbinations([1,2,3,4,7], 11))
 
 onMounted(() => {
     synth = new Tone.Sampler({
--- shared/temperament.ts
+++ shared/temperament.ts
@@ -1,19 +1,29 @@
 const basicKeys: (keyof BasicInotonationSequence)[] = ["A", "B", "C", "D", "E", "F", "G"]
 const inotonations: (keyof Inotonation)[] = ["Just", "Equal"]
-var baseFreq = 440
-
-enum Key {
-    "A", "A#", "Bb", "B", "C", "C#", "Db", "D", "D#", "Eb", "E", "F", "F#", "Gb", "G", "G#", "Ab"
+const allKeys: ((keyof BasicInotonationSequence) | string)[] = ["A", "A#", "Bb", "B", "C", "C#", "Db", "D", "D#", "Eb", "E", "F", "F#", "Gb", "G", "G#", "Ab"]
+var baseFreq = 220
+export enum Key {
+    "A" = 0, "A#" = 1, "Bb" = 1, "B" = 2, "C" = 3, "C#" = 4, "Db" = 4, "D" = 5, "D#" = 6, "Eb" = 6, "E" = 7, "F" = 8, "F#" = 9, "Gb" = 9, "G" = 10, "G#" = 11, "Ab" = 11
 }
+export var baseKey: Key | null = null
 
 type Inotonation = {
     Just: FreqBlock,
     Equal: FreqBlock
 }
 
+interface Ins { [Key: number]: number; }
+
 type FreqBlock = {
     basic: BasicInotonationSequence,
-    intervals: Intervals
+    basicFreqs?: BasicFreqs
+    intervals: Interval,
+}
+
+type Interval = {
+    ins: Ins,
+    detect: boolean,
+    seqs: number[]
 }
 
 type BasicInotonationSequence = {
@@ -26,13 +36,7 @@ type BasicInotonationSequence = {
     G: [keyof BasicInotonationSequence, number],
 }
 
-type Intervals = {
-    mn2: number,
-    mj2: number,
-    mn3: number,
-    mj3: number,
-    pe5: number,
-}
+type BasicFreqs = { A: number, B: number, C: number, D: number, E: number, F: number, G: number }
 
 type FreqEcof = [keyof BasicInotonationSequence, number]
 
@@ -40,53 +44,122 @@ const inotons: Inotonation = {
     Just: {
         basic: {
             A: ["A", 1],
+            B: ["A", 1.125],
+            C: ["A", 1.2],
+            D: ["C", 9 / 8],
+            E: ["C", 5 / 4],
+            F: ["C", 4 / 3],
+            G: ["C", 3 / 2],         
+        },
+        intervals: {
+            ins: {
+                1: 16/15,       // minor second
+                2: 9/8,         // major second
+                3: 6/5,         // minor third
+                4: 5/4,         // major third
+                7: 3/2,         // perfect fifth
+            },
+            detect: true,
+            seqs: [7, 4, 3, 2, 1]
+        }
+    },
+    Equal: {
+        basic: {
+            A: ["A", 1],
             B: ["A", Math.pow(2, 1 / 12)],
-            C: ["A", Math.pow(2, 2 / 12)],
+            C: ["A", Math.pow(2, 3 / 12)],
             D: ["C", Math.pow(2, 2 / 12)],
             E: ["C", Math.pow(2, 4 / 12)],
             F: ["C", Math.pow(2, 5 / 12)],
             G: ["C", Math.pow(2, 7 / 12)],
         },
         intervals: {
-            mn2: 16/15,
-            mj2: 9/8,
-            mn3: 6/5,
-            mj3: 5/4,
-            pe5: 3/2,
-        }
-    },
-    Equal: {
-        basic: {
-            A: ["A", 1],
-            B: ["A", 1.125],
-            C: ["A", 1.2],
-            D: ["C", 9 / 8],
-            E: ["C", 5 / 4],
-            F: ["C", 4 / 3],
-            G: ["C", 3 / 2],
-        },
-        intervals: {
-            mn2: Math.pow(2, 1/12),
-            mj2: Math.pow(2, 2/12),
-            mn3: Math.pow(2, 3/12),
-            mj3: Math.pow(2, 4/12),
-            pe5: Math.pow(2, 7/12)
+            ins: { 1: Math.pow(2, 1/12) },
+            detect: false,
+            seqs: [1]
         }
     }
 }
 
-const freqs = (tones: [string], key: Key): number[] => {
-
+const freqs = (tones: [string]): number[] => {
     var res: number[] = []
+    // for (const t in tones) {
+    //     const key = t.slice(0, -1)
+    //     if (key in just) {
+    //         // 全音
 
-    for (const t in tones) {
-        const key = t.slice(0, -1)
-        if (key in just) {
-            // 全音
+    //     }
+    // }
+    return []
+}
 
+const getKeyTransferRatio = (key: Key, freqs: BasicFreqs, intervals: Interval): number => {
+
+    if (allKeys[key] in freqs) {
+        
+        
+
+
+
+    } else {
+        if (intervals.detect) {
+            if (key in intervals.ins) {
+                return intervals.ins[key]
+            } else {
+                return 0
+            }
+        } else {
+            return Math.pow(intervals.ins[1], key)
         }
     }
+}
 
+export const conbinations = (candidates: number[], target: number): number[] => {
+    const ans: number[][] = []
+    var t = 5
+    var smallest = candidates.length
+    const dfs = (target: number, combine: number[], idx: number) => {
+        if (t === 0) return
+        if (idx === candidates.length) return
+        if (target === 0) {
+            t--
+            if (combine.length < smallest) smallest = combine.length
+            ans.push(combine)
+            return
+        }
+        // 直接跳过
+        dfs(target, combine, idx + 1)
+        // 选择当前数
+        if (target - candidates[idx] >= 0) {
+            dfs(target - candidates[idx], [candidates[idx], ...combine], idx)
+        }
+    }
+    dfs(target, [], 0)
+    return ans.filter(a => a.length === smallest).sort((a, b) => b[0] - a[0])[0]
+}
 
-    return []
+export const updateBasicInotonations = (key: Key): void => {
+    if (baseKey === key) return
+    baseKey = key
+    var basicFs: BasicFreqs[] = []
+    for (const name of inotonations) {
+        basicFs.push({A: baseFreq, B: 0, C: 0, D: 0, E: 0, F: 0, G: 0})
+    }
+    for (const key of basicKeys) {
+        if (key === "A") continue
+        for (const i in inotonations) {
+            const freqEcof = inotons[inotonations[i]].basic[key]
+            basicFs[i][key] = basicFs[i][freqEcof[0]] * freqEcof[1]   
+        }
+    }
+    for (const i in basicFs) {
+        const curIon = inotons[inotonations[i]]
+        const ratio = curIon.intervals.detect ? curIon.intervals.ins[key] : Math.pow(curIon.intervals.ins[1], key)
+        
+        
+
+        curIon.basicFreqs = basicFs[i]
+    }
+
+    console.log(inotons)
 }
